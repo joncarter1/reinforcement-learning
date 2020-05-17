11450882e2501239f05f5b78ec56e232950bfd6b
jfc43 <jfc43@cam.ac.uk>
2020-05-17 10:35:30 +0100

    n step ac script

--- ModelFree/ActorCritic/OneStepAC.py
+++ ModelFree/ActorCritic/OneStepAC.py
@@ -17,26 +17,28 @@ from copy import deepcopy
 import random
 
 class PolicyEstimator:
-    def __init__(self, env, name=None, gamma=0.99, normalise=True):
+    def __init__(self, env, name=None, gamma=0.99, n_steps=5):
         # Environment specifics
         self.env = env
-        self.normalise = normalise
         self.env_name = env.unwrapped.spec.id
         self.gamma = gamma
         self.G_n, self.advantages = 0, 0
+        self.n_steps = n_steps
         self.input_shape = env.observation_space.shape
         self.n_outputs = env.action_space.n
         self.action_space = [i for i in range(self.n_outputs)]
         # Neural network architecture
-        self.l1_dims = 250
-        self.l2_dims = 250
+        self.p1_dims = 50
+        self.p2_dims = 50
+        self.v1_dims = 250
+        self.v2_dims = 250
         self.lr = 1e-3
         # Memory
-        self.states, self.actions, self.rewards, self.next_states = [], [], [], []
+        self.states, self.actions, self.rewards, self.next_states, self.g_ns = [], [], [], [], []
         self.REPLAY_MEMORY = deque(maxlen=10000)
         self.MIN_REPLAY_MEMORY_SIZE = 1000  # Minimum number of steps in a memory to start training
-        self.UPDATE_TARGET_EVERY = 5  # How often to sync. target and true value networks
-        self.MINIBATCH_SIZE=32
+        self.UPDATE_TARGET_EVERY = 2  # How often to sync. target and true value networks
+        self.MINIBATCH_SIZE = 32
         self.target_update_counter = 0
         # Stats for tracking performance
         self.scores = []
@@ -48,20 +50,20 @@ class PolicyEstimator:
 
         if name:
             subdir_name = "/".join([self.env_name, name])
-            self.scores = pickle.load(open("{}/scores.p","rb".format(subdir_name)))
+            self.scores = pickle.load(open("{}/scores.p".format(subdir_name),"rb"))
             self.policy_nn.load_weights("{}/PolicyNN-weights.h5".format(subdir_name))
             self.predict_nn.load_weights("{}/PredictNN-weights.h5".format(subdir_name))
 
     def create_models(self):
         input = Input(shape=self.input_shape)
         advantages = Input(shape=[1])
-        dense1 = Dense(self.l1_dims, activation="relu")(input)
-        dense2 = Dense(self.l2_dims, activation="relu")(dense1)
+        dense1 = Dense(self.p1_dims, activation="relu")(input)
+        dense2 = Dense(self.p2_dims, activation="relu")(dense1)
         probs = Dense(self.n_outputs, activation="softmax")(dense2)
 
         #v_input = Input(shape=self.input_shape)
-        v_dense1 = Dense(self.l1_dims, activation="relu")(input)
-        v_dense2 = Dense(self.l2_dims, activation="relu")(v_dense1)
+        v_dense1 = Dense(self.v1_dims, activation="relu")(input)
+        v_dense2 = Dense(self.v2_dims, activation="relu")(v_dense1)
         v_out = Dense(1, activation="linear")(v_dense2)
         value_nn = Model(input=[input], output=[v_out])
         value_nn.compile(optimizer="rmsprop", loss='mse')
@@ -102,17 +104,22 @@ class PolicyEstimator:
         action_memory = np.array(self.actions)
         reward_memory = np.array(self.rewards)
         next_state_memory = np.array(self.next_states)
+        discount_filter = np.flip(self.gamma ** np.arange(0, self.n_steps))
+        G_n = np.convolve(reward_memory, discount_filter)[-reward_memory.shape[0]:]  # n-step return before value fn.
+        # Append zeros for last n states, v(s_end) = 0
+        state_vals = np.hstack((np.squeeze(self.value_nn.predict(state_memory[self.n_steps:])), np.zeros(self.n_steps)))
+        G_n += (self.gamma**self.n_steps)*state_vals  # n-step value function added to give full n-step return
+
         # One hot encoding of actions
         actions = np.zeros([len(action_memory), self.n_outputs])
         actions[np.arange(len(action_memory)), action_memory] = 1
         end = np.ones_like(reward_memory)
         end[-1] = 0
 
-        # One step actor-critic td errors as in Sutton-Barto, we times by 'done' vector [1,1,1,...,1,0] so v(s_end) = 0
-        value_targets = reward_memory + self.gamma*np.squeeze(self.value_nn.predict(next_state_memory))*end
-        td_errors = value_targets - np.squeeze(self.value_nn.predict(state_memory))
+        # n step actor-critic td errors as in Sutton-Barto
+        td_errors = G_n - np.squeeze(self.value_nn.predict(state_memory))
 
-        #self.value_nn.train_on_batch(state_memory, value_targets)
+        self.value_nn.train_on_batch(state_memory, G_n)
         self.policy_nn.train_on_batch([state_memory, td_errors], actions)
 
         self.clear_transitions()
@@ -163,16 +170,16 @@ class PolicyEstimator:
         with open("{}/specs.txt".format(subdir_name), "a") as text_file:
             text_file.write("lr = {}\n".format(self.lr))
             text_file.write("gamma = {}\n".format(self.gamma))
-            text_file.write("normalise = {}\n".format(self.normalise))
+            text_file.write("n_steps = {}\n".format(self.n_steps))
         self.save_model(subdir_name)
 
 
-def main(EPISODES=3000, LOAD_MODEL=None, save_name=None,  normalise=False, gamma=0.99, seed=1):
+def main(EPISODES=3000, LOAD_MODEL=None, save_name=None,  n_steps=5, gamma=0.99, seed=1):
     env = gym.make('LunarLander-v2')
     np.random.seed(seed)
     env.seed(seed)
     # LOAD_MODEL = "NB0.99"
-    agent = PolicyEstimator(env, LOAD_MODEL, gamma, normalise)
+    agent = PolicyEstimator(env, LOAD_MODEL, gamma, n_steps)
 
     # Iterate over episodes
 
@@ -183,21 +190,23 @@ def main(EPISODES=3000, LOAD_MODEL=None, save_name=None,  normalise=False, gamma
         if not episode % (EPISODES//10) and LOAD_MODEL:
             show = True
         current_state = env.reset()
-        score, no_steps = 0, 0
+        score, no_steps, discount = 0, 0, 1
         # Reset flag and start iterating until episode ends
         done = False
         start = timer()
+        discounted_sum = 0
         while not done:
-            no_steps += 1  # Max steps
             action = agent.choose_action(current_state)
             next_state, reward, done, _ = env.step(action)
+
             if show:
                 env.render()
             agent.store_transition(current_state, action, reward, next_state, done)
-            if random.random() < 0.4:
-                agent.train(done)
+            #if random.random() < 0.5:
+            #    agent.train(done)
             current_state = next_state
             score += reward
+            no_steps += 1  # Max steps
         agent.update_stats(score)
         ep_time = timer() - start
         steps.append(no_steps)
@@ -209,13 +218,13 @@ def main(EPISODES=3000, LOAD_MODEL=None, save_name=None,  normalise=False, gamma
         if not episode % 10:
             print(ep_time, "seconds of episode")
             print(training_time, "to train")
-            print("{} score, mean score {}, {} mean no. steps".format(score, np.mean(scores[-100:]), np.mean(steps[-100:])))
+            print("{} score, mean score {}, {} mean no. steps".format(score, np.mean(scores[-50:]), np.mean(steps[-50:])))
     if save_name:
         agent.save_all(save_name)
 
 
 if __name__ == "__main__":
-    normalise, gamma = False, 0.97
+    n_steps, gamma = 10, 0.99
     load_model, save_model = "AC99", None
-    load_model, save_model = None, "AC97"
-    main(1500, load_model, save_model, normalise=normalise, gamma=gamma)
+    load_model, save_model = None, "AC-10-99"
+    main(2000, load_model, save_model, n_steps=n_steps, gamma=gamma)
